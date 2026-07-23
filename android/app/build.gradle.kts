e168faf322410b656004c56542f76579be1834b0
tholeteplok <teplok85@gmail.com>
2026-07-24 05:39:34 +0700

    fix: sinkronisasi namespace (com.mekaar.mekaar_chat) dan explicit MainActivity pada AndroidManifest.xml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.mekaar.app"
+    namespace = "com.mekaar.mekaar_chat"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
